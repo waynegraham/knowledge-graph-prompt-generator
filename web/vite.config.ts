5f4a5a1023679dbd6020b6e3708402b59cf6d180
Wayne Graham <wgraham@clir.org>
2026-02-02 10:00:29 -0500

    Update config for tutorial

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: './',
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tutorial: resolve(__dirname, 'tutorial.html'),
+        themePreview: resolve(__dirname, 'theme-preview.html'),
+      },
+    },
+  },
 })
